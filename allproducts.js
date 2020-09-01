bd3ff67687a2e146abc5331fcc8b13fbfb52d794
sreejohn <johnpitta321@gmail.com>
2020-09-01 14:43:19 +0530

    Add files via upload

---
+++ allproducts.js
@@ -0,0 +1,364 @@
+// ************************************************
+// Shopping Cart API
+// ************************************************
+
+var shoppingCart = (function () {
+  // =============================
+  // Private methods and propeties
+  // =============================
+  cart = [];
+
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+
+  // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+
+
+  // =============================
+  // Public methods and propeties
+  // =============================
+  var obj = {};
+
+  // Add to cart
+  obj.addItemToCart = function (name, price, count) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count++;
+        saveCart();
+        return;
+      }
+    }
+    var item = new Item(name, price, count);
+    cart.push(item);
+    saveCart();
+  }
+  // Set count from item
+  obj.setCountForItem = function (name, count) {
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count = count;
+        break;
+      }
+    }
+  };
+  // Remove item from cart
+  obj.removeItemFromCart = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count--;
+        if (cart[item].count === 0) {
+          cart.splice(item, 1);
+        }
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function () {
+    cart = [];
+    saveCart();
+  }
+
+  // Count cart 
+  obj.totalCount = function () {
+    var totalCount = 0;
+    for (var item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  }
+
+  // Total cart
+  obj.totalCart = function () {
+    var totalCart = 0;
+    for (var item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+  // List cart
+  obj.listCart = function () {
+    var cartCopy = [];
+    for (i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for (p in item) {
+        itemCopy[p] = item[p];
+
+      }
+      itemCopy.total = Number(item.price * item.count).toFixed(2);
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }
+
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
+  return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+$('.add-to-cart').click(function (event) {
+  // alert("cle");
+  event.preventDefault();
+  var name = $(this).data('name');
+  var price = Number($(this).data('price'));
+  shoppingCart.addItemToCart(name, price, 1);
+  // alert("Item added to cart successfully");
+  $(".myAlert-top").show();
+  setTimeout(function () {
+    $(".myAlert-top").hide();
+  }, 5000);
+  displayCart();
+});
+
+// Clear items
+$('.clear-cart').click(function () {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for (var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>"
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+      + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      + " = "
+      + "<td>" + cartArray[i].total + "</td>"
+      + "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function (event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCartAll(name);
+  displayCart();
+})
+
+
+// -1
+$('.show-cart').on("click", ".minus-item", function (event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCart(name);
+  displayCart();
+})
+// +1
+$('.show-cart').on("click", ".plus-item", function (event) {
+  var name = $(this).data('name')
+  shoppingCart.addItemToCart(name);
+  displayCart();
+})
+
+// Item count input
+$('.show-cart').on("change", ".item-count", function (event) {
+  var name = $(this).data('name');
+  var count = Number($(this).val());
+  shoppingCart.setCountForItem(name, count);
+  displayCart();
+});
+function ViewProductDetails(product_id) {
+  var AllProducts = [];
+  if (localStorage && localStorage.getItem('AllProducts') != undefined) {
+    AllProducts = JSON.parse((localStorage.getItem('AllProducts')));
+  }
+
+
+  for (var j = 0; j < AllProducts.length; j++) {
+    if (AllProducts[j].product_id == product_id) {
+      localStorage.setItem('Productdetails', JSON.stringify(AllProducts[j]));
+      window.location.href = "/Productdescription.html";
+
+      //break;
+    }
+  }
+}
+
+
+
+displayCart();
+
+
+
+
+// Returns an array of maxLength (or less) page numbers
+// where a 0 in the returned array denotes a gap in the series.
+// Parameters:
+//   totalPages:     total number of pages
+//   page:           current page
+//   maxLength:      maximum size of returned array
+function getPageList(totalPages, page, maxLength) {
+  if (maxLength < 5) throw "maxLength must be at least 5";
+
+  function range(start, end) {
+    return Array.from(Array(end - start + 1), (_, i) => i + start);
+  }
+
+  var sideWidth = maxLength < 9 ? 1 : 2;
+  var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+  var rightWidth = (maxLength - sideWidth * 2 - 2) >> 1;
+  if (totalPages <= maxLength) {
+    // no breaks in list
+    return range(1, totalPages);
+  }
+  if (page <= maxLength - sideWidth - 1 - rightWidth) {
+    // no break on left of page
+    return range(1, maxLength - sideWidth - 1)
+      .concat([0])
+      .concat(range(totalPages - sideWidth + 1, totalPages));
+  }
+  if (page >= totalPages - sideWidth - 1 - rightWidth) {
+    // no break on right of page
+    return range(1, sideWidth)
+      .concat([0])
+      .concat(
+        range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages)
+      );
+  }
+  // Breaks on both sides
+  return range(1, sideWidth)
+    .concat([0])
+    .concat(range(page - leftWidth, page + rightWidth))
+    .concat([0])
+    .concat(range(totalPages - sideWidth + 1, totalPages));
+}
+
+$(function() {
+  // Number of items and limits the number of items per page
+  var AllProducts = [];
+  if (localStorage && localStorage.getItem('AllProducts') != undefined) {
+    AllProducts = JSON.parse((localStorage.getItem('AllProducts')));
+  }
+ // var numberOfItems = $("#jar .col").length;
+  var numberOfItems=AllProducts.length;
+  var limitPerPage = 6;
+  // Total pages rounded upwards
+  var totalPages = Math.ceil(numberOfItems / limitPerPage);
+  // Number of buttons at the top, not counting prev/next,
+  // but including the dotted buttons.
+  // Must be at least 5:
+  var paginationSize = 7;
+  var currentPage;
+
+  function showPage(whichPage) {
+    if (whichPage < 1 || whichPage > totalPages) return false;
+    currentPage = whichPage;
+    $("#jar .col")
+      .hide()
+      .slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage)
+      .show();
+    // Replace the navigation items (not prev/next):
+    $(".pagination li").slice(1, -1).remove();
+    getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+      $("<li>")
+        .addClass(
+          "page-item " +
+            (item ? "current-page " : "") +
+            (item === currentPage ? "active " : "")
+        )
+        .append(
+          $("<a>")
+            .addClass("page-link")
+            .attr({
+              href: "javascript:void(0)"
+            })
+            .text(item || "...")
+        )
+        .insertBefore("#next-page");
+    });
+    return true;
+  }
+
+  // Include the prev/next buttons:
+  $(".pagination").append(
+    $("<li>").addClass("page-item").attr({ id: "previous-page" }).append(
+      $("<a>")
+        .addClass("page-link")
+        .attr({
+          href: "javascript:void(0)"
+        })
+        .text("Prev")
+    ),
+    $("<li>").addClass("page-item").attr({ id: "next-page" }).append(
+      $("<a>")
+        .addClass("page-link")
+        .attr({
+          href: "javascript:void(0)"
+        })
+        .text("Next")
+    )
+  );
+  // Show the page links
+  $("#test12").show();
+  showPage(1);
+
+  // Use event delegation, as these items are recreated later
+  $(
+    document
+  ).on("click", ".pagination li.current-page:not(.active)", function() {
+    return showPage(+$(this).text());
+  });
+  $("#next-page").on("click", function() {
+    return showPage(currentPage + 1);
+  });
+
+  $("#previous-page").on("click", function() {
+    return showPage(currentPage - 1);
+  });
+  $(".pagination").on("click", function() {
+    $("html,body").animate({ scrollTop: 0 }, 0);
+  });
+});
